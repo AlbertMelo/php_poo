d74925ec22966a0a4b48e68683f25434047291c5
albert-melo <aldm.cid20@uea.edu.br>
2021-11-19 20:25:59 -0300

    PHP POO - Classes

--- src/Conta.php
+++ src/Conta.php
@@ -1,6 +1,42 @@
 <?php
 
-class Conta {
+class Conta
+{
 
-    //Definir os dados da conta
+    private string $cpfTitular;
+
+    private string $nomeTitular;
+
+    private float $saldo;
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Fundos insuficientes para realizar a operação.";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor inválido para depósito!";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
