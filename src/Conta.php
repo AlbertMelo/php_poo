43f7f210290a451f0168fd28b34edeceef7c3947
albert-melo <aldm.cid20@uea.edu.br>
2021-11-19 21:31:51 -0300

    PHP POO - Abstracao e Encapsulamento

--- src/Conta.php
+++ src/Conta.php
@@ -7,36 +7,61 @@ class Conta
 
     private string $nomeTitular;
 
-    private float $saldo;
+    private float $saldo = 0;
 
-    public function sacar(float $valorASacar): void
+    public function saca(float $valorASacar): void
     {
         if ($valorASacar > $this->saldo) {
-            echo "Fundos insuficientes para realizar a operação!";
+            echo "Saldo indisponível";
             return;
         }
 
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
         if ($valorADepositar < 0) {
-            echo "Valor inválido para depósito!";
+            echo "Valor precisa ser positivo";
             return;
         }
 
         $this->saldo += $valorADepositar;
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
         if ($valorATransferir > $this->saldo) {
-            echo "Fundos insuficientes para realizar a operação!";
+            echo "Saldo indisponível";
             return;
         }
 
         $this->sacar($valorATransferir);
         $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function defineCpfTitular(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function defineNomeTitular(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->nomeTitular;
     }
 }
